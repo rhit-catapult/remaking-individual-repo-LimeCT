1455f9036bec71da757995a6ecd581a7bbbd55e2
LimeCT <tribblecolin4@gmail.com>
2025-07-11 10:49:04 -0500

    3 balls bouncing

--- 05-BouncingBall/BouncingBall.py
+++ 05-BouncingBall/BouncingBall.py
@@ -1,45 +1,93 @@
-import pygame
 import sys
-import random
 
+import pygame
+import math
+import random
 
 # You will implement this module ENTIRELY ON YOUR OWN!
 
 # TODO: Create a Ball class.
-# TODO: Possible member variables: screen, color, x, y, radius, speed_x, speed_y
-# TODO: Methods: __init__, draw, move
+class Ball:
+    def __init__(self, screen, x, y):
+        self.screen = screen
+        self.x = x
+        self.y = y
+        self.speed= random.randint(5, 25)
+        self.radius= 50
+        self.color= (154, 58, 212)
+        self.speed_y = 5
+        self.speed_x = 5
+
+
+    def move(self):
+        self.y += self.speed_y
+
+    def bounce(self):
+        self.x += self.speed_x
+        self.y += self.speed_y
+
+
+        if self.x - self.radius <= 0 or self.x + self.radius >= 1000:
+            self.speed_x = -self.speed_x
+
+        if self.y - self.radius <= 0 or self.y + self.radius >= 600:
+            self.speed_y = -self.speed_y
+
+    def draw(self):
+        pygame.draw.circle(self.screen, (154, 58, 212), (self.x, self.y), 50, 3)
+
+    def check_collision(self, Ball2):
+        distance = math.sqrt((self.x - Ball2.x) ** 2 + (self.y - Ball2.y) ** 2)
+        return distance <= self.radius + Ball2.radius
+
+    def bounce_off(self, Ball2):
+        if self.check_collision(Ball2):
+            self.speed_x, Ball2.speed_x = -self.speed_x, -Ball2.speed_x
+            self.speed_y, Ball2.speed_y = -self.speed_y, -Ball2.speed_y
+
 
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((300, 300))
+    screen = pygame.display.set_mode((1000, 600))
     pygame.display.set_caption('Bouncing Ball')
-    screen.fill(pygame.Color('gray'))
     clock = pygame.time.Clock()
 
-    # TODO: Create an instance of the Ball class called ball1
+    # Create two Ball instances using the unified Ball class
+    ball1 = Ball(screen, 320, 100)
+    ball2 = Ball(screen, 550, 100)
+    ball3 = Ball(screen, 430, 350)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                pygame.quit()
                 sys.exit()
 
-        clock.tick(60)
         screen.fill(pygame.Color('gray'))
 
-        # TODO: Move the ball
-        # TODO: Draw the ball
+        # Move both balls
+        ball1.move()
+        ball2.move()
+        ball3.move()
+
+        # Check and respond to collisions between the two balls
+        ball1.bounce_off(ball2)
+        ball1.bounce_off(ball3)
+        ball2.bounce_off(ball3)
+
+
+        # Bounce off walls
+        ball1.bounce()
+        ball2.bounce()
+        ball3.bounce()
+
+        # Draw both balls
+        ball1.draw()
+        ball2.draw()
+        ball3.draw()
 
         pygame.display.update()
-
+        clock.tick(60)
 
 main()
-
-
-# Optional challenges (if you finish and want do play a bit more):
-#   After you get 1 ball working make a few balls (ball2, ball3, etc) that start in different places.
-#   Make each ball a different color
-#   Make the screen 1000 x 800 to allow your balls more space (what needs to change?)
-#   Make the speed of each ball randomly chosen (1 to 5)
-#   After you get that working try making a list of balls to have 100 balls (use a loop)!
-#   Use random colors for each ball
